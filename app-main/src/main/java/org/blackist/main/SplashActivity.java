618fe9b717bc426e4c99c9598c9c239055f74b11
LiangLiang.Dong <liangl.dong@qq.com>
2018-09-29 16:39:18 +0800

    [Module]: 统一第三库依赖库版本；加入模块资源前缀，表面资源冲突；统一混淆配置；

--- app-main/src/main/java/org/blackist/main/SplashActivity.java
+++ app-main/src/main/java/org/blackist/main/SplashActivity.java
@@ -20,6 +20,7 @@ public class SplashActivity extends BaseActivity {
             @Override
             public void run() {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
             }
         }, 3000);
     }
