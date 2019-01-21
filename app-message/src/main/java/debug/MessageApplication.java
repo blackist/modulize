5a69988d5bd6d2bf58f735e15084451b8613769d
LiangLiang.Dong <liangl.dong@gmail.com>
2019-01-21 09:43:10 +0800

    [Message] add Application

---
+++ app-message/src/main/java/debug/MessageApplication.java
@@ -0,0 +1,12 @@
+package debug;
+
+import org.blackist.common.context.CommonApplication;
+
+/**
+ * @author LiangLiang.Dong[liangl.dong@gmail.com]
+ * @since 2019/1/21
+ */
+
+public class MessageApplication extends CommonApplication {
+    
+}
